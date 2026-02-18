7171b38bab1a1196c6aeb59f15c552d9c9fca45d
Sadyk Tamerlan <temirlan.sadyk2008@gmail.com>
2026-02-19 03:16:34 +0500

    in proccess

---
+++ Practice4/math.py
@@ -0,0 +1,7 @@
+import math
+
+print("Square root:", math.sqrt(25))
+print("Power:", math.pow(2, 3))
+print("Absolute:", math.fabs(-7.5))
+print("Ceil:", math.ceil(4.3))
+print("Floor:", math.floor(4.9))
